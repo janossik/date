733ca3f857b40818356b8290ed5da2b8cd46ede8
Marcin Czaniecki <marcinczaniecki97@gmail.com>
2021-09-30 03:55:11 +0200

    addLeadingZero and update type

--- src/index.ts
+++ src/index.ts
@@ -42,43 +42,43 @@ export const numberMonthToNameMonth = (month: number, lang?: string[]) => {
   let name = '';
   switch (month) {
     case 0:
-      name = lang ? lang[0] : 'January';
+      name = lang ? lang[0] : 'january';
       break;
     case 1:
-      name = lang ? lang[1] : 'February';
+      name = lang ? lang[1] : 'february';
       break;
     case 2:
-      name = lang ? lang[2] : 'March';
+      name = lang ? lang[2] : 'march';
       break;
     case 3:
-      name = lang ? lang[3] : 'April';
+      name = lang ? lang[3] : 'april';
       break;
     case 4:
-      name = lang ? lang[4] : 'May';
+      name = lang ? lang[4] : 'may';
       break;
     case 5:
-      name = lang ? lang[5] : 'June';
+      name = lang ? lang[5] : 'june';
       break;
     case 6:
-      name = lang ? lang[6] : 'July';
+      name = lang ? lang[6] : 'july';
       break;
     case 7:
-      name = lang ? lang[7] : 'August';
+      name = lang ? lang[7] : 'august';
       break;
     case 8:
-      name = lang ? lang[8] : 'September';
+      name = lang ? lang[8] : 'september';
       break;
     case 9:
-      name = lang ? lang[9] : 'October';
+      name = lang ? lang[9] : 'october';
       break;
     case 10:
-      name = lang ? lang[10] : 'November';
+      name = lang ? lang[10] : 'november';
       break;
     case 11:
-      name = lang ? lang[11] : 'December';
+      name = lang ? lang[11] : 'december';
       break;
     default:
-      throw Error('You must enter a number from 1 to 12');
+      throw Error('You must enter a number from 0 to 11');
   }
   return name;
 };
@@ -90,7 +90,7 @@ type TypeMakeMonth = (
   lang?: string[],
 ) => {
   name: string;
-  days: any[];
+  days: { id: string; day: number; visible: boolean }[];
 };
 
 export const getMonthArray: TypeMakeMonth = (year, month, day, lang) => {
@@ -122,23 +122,11 @@ export const getMonthArray: TypeMakeMonth = (year, month, day, lang) => {
     days,
   };
 };
+export const addLeadingZero = (number: number): string => (number < 10 ? `0${number}` : `${number}`);
 
 export const printDDMMYYYY = (date: Date) => {
   const year = date.getFullYear();
   const month = date.getMonth() + 1;
   const day = date.getDate();
-  let ddmmyyyy = '';
-
-  if (day < 10) {
-    ddmmyyyy += `0${day}.`;
-  } else {
-    ddmmyyyy += `${day}.`;
-  }
-  if (month < 10) {
-    ddmmyyyy += `0${month}.`;
-  } else {
-    ddmmyyyy += `${month}.`;
-  }
-  ddmmyyyy += `${year}`;
-  return ddmmyyyy;
+  return `${addLeadingZero(day)}.${addLeadingZero(month)}.${year}`;
 };
